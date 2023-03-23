0ddf3a98168fdfcc15b4c1236073901163eabc69
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-03-23 14:22:45 +0100

    Initialize popup and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { UnderlineNav } from "@primer/react/drafts";
+
+export default function Navbar() {
+  return (
+    <div>
+      <UnderlineNav aria-label="Repository">
+        <UnderlineNav.Item aria-current="page" icon={CodeIcon}>
+          Code
+        </UnderlineNav.Item>
+        <UnderlineNav.Item icon={IssueOpenedIcon} counter={30}>
+          Issues
+        </UnderlineNav.Item>
+        <UnderlineNav.Item icon={GitPullRequestIcon} counter={3}>
+          Pull Requests
+        </UnderlineNav.Item>
+        <UnderlineNav.Item icon={CommentDiscussionIcon}>
+          Discussions
+        </UnderlineNav.Item>
+        <UnderlineNav.Item icon={EyeIcon} counter={9}>
+          Actions
+        </UnderlineNav.Item>
+        <UnderlineNav.Item icon={EyeIcon} counter={7}>
+          Projects
+        </UnderlineNav.Item>
+      </UnderlineNav>
+    </div>
+  );
+}
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Popup() {
-  return <h1>Hallos</h1>;
+  return (
+    <div>
+      <h1>Overskrift</h1>
+      <Navbar />
+    </div>
+  );
 }
