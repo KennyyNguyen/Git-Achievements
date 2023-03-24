db1458fe53f1d74fe2240edde8a5e2d8910bca72
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-03-24 12:17:38 +0100

    Wrap app in theme for Primer React to work correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Popup from "./components/Popup";
+import { BaseStyles, ThemeProvider } from "@primer/react";
 
 function App() {
   return (
-    <div className="App">
-      <Popup />
-    </div>
+    <ThemeProvider>
+      <BaseStyles>
+        <div className="App">
+          <Popup />
+        </div>
+      </BaseStyles>
+    </ThemeProvider>
   );
 }
 
