b74cba72d5dc91486baa1bbf0615f7aef36fb15e
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-03-24 13:34:29 +0100

    Remove Github Primer design system

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import Popup from "./components/Popup";
-import { BaseStyles, ThemeProvider } from "@primer/react";
 
 function App() {
   return (
-    <ThemeProvider>
-      <BaseStyles>
-        <div className="App">
-          <Popup />
-        </div>
-      </BaseStyles>
-    </ThemeProvider>
+    <div className="App">
+      <Popup />
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,9 @@
 import React from "react";
-import { UnderlineNav } from "@primer/react/drafts";
-import {
-  CodeIcon,
-  IssueOpenedIcon,
-  GitPullRequestIcon,
-  CommentDiscussionIcon,
-  EyeIcon,
-} from "@primer/octicons-react";
 
 export default function Navbar() {
   return (
     <div>
-      <UnderlineNav aria-label="Repository">
-        <UnderlineNav.Item aria-current="page" icon={CodeIcon}>
-          Code
-        </UnderlineNav.Item>
-        <UnderlineNav.Item icon={IssueOpenedIcon} counter={30}>
-          Issues
-        </UnderlineNav.Item>
-        <UnderlineNav.Item icon={GitPullRequestIcon} counter={3}>
-          Pull Requests
-        </UnderlineNav.Item>
-        <UnderlineNav.Item icon={CommentDiscussionIcon}>
-          Discussions
-        </UnderlineNav.Item>
-        <UnderlineNav.Item icon={EyeIcon} counter={9}>
-          Actions
-        </UnderlineNav.Item>
-        <UnderlineNav.Item icon={EyeIcon} counter={7}>
-          Projects
-        </UnderlineNav.Item>
-      </UnderlineNav>
+      <h1>Hallo</h1>
     </div>
   );
 }
