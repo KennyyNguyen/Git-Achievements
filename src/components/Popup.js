48ddaa067cef51bec5d3247e86adafc98cc3c682
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-03-28 15:09:32 +0200

    Initialize new profile card component

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Box, Container, Divider, Flex } from "@chakra-ui/react";
 import Navbar from "./Navbar";
+import ProfileDisplay from "./ProfileDisplay";
 
 export default function Popup() {
   return (
     <>
       <Container bg="red.200">
         <Flex bg="purple.100">
-          <Box bg="blue.100">Box1</Box>
+          <ProfileDisplay />
           <Divider orientation="vertical" />
           <Box bg="blue">
             Box2
---
+++ src/components/ProfileDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProfileDisplay() {
+  return (
+    <>
+      <h1>Her skal vi vise profilbilde og recently achivements</h1>
+    </>
+  );
+}
