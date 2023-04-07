387f15a891e6731d542fc2bfc1c8a56b906da911
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-07 20:07:08 +0200

    Remove unused imports

--- src/components/ProfileDisplay.js
+++ src/components/ProfileDisplay.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Center, Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import UserAvatar from "./UserAvatar";
 import UserName from "./UserName";
 
