905ef065108838786dd6a101fba02cdc9f3e7bd3
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-08 12:54:11 +0200

    Add profile component to be displayed on main page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { RiKey2Fill, RiGitlabFill } from "react-icons/ri";
 
 export default function Profile() {
-  return <h1>Hallo denne siden skal inneholde profilen</h1>;
+  const [show, setShow] = useState("");
+  const handleClick = () => setShow(!show);
+
+  return (
+    <Flex direction="column">
+      <Heading>Profile</Heading>
+      <FormControl>
+        <FormLabel>Personal GitLab Token</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<Icon as={RiKey2Fill} color="gray.400" />}
+          />
+          <Input
+            placeholde="Your GitLab token"
+            type={show ? "text" : "password"}
+          />
+          <InputRightElement>
+            <Button h="1.75rem" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <FormLabel>GitLab Host Address</FormLabel>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<Icon as={RiGitlabFill} color="gray.400" />}
+          />
+          <Input placeholder="Ex: https://gitlab.stud.idi.ntnu.no/api/v4/" />
+        </InputGroup>
+      </FormControl>
+    </Flex>
+  );
 }
