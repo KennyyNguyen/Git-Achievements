bdee383ab6b68c25c4d66c68cbae32dd508bf726
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-11 13:36:02 +0200

    Add parameters to GitLab API function

--- src/api/initGitlabApi.js
+++ src/api/initGitlabApi.js
@@ -1,6 +1,7 @@
 import { Gitlab } from "@gitbeaker/browser";
 
-export const api = new Gitlab({
-  host: "https://gitlab.stud.idi.ntnu.no",
-  token: "glpat-9P2csQz7hVezQsU3yj-g",
-});
+export const api = (host, token) =>
+  new Gitlab({
+    host: host,
+    token: token,
+  });
