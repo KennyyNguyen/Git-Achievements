27ea3239576359a829dea1dd2a2ce2d7dc12836e
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-17 10:58:08 +0200

    Add test alert message and initiated fetching gitlab data

---
+++ src/background/endpoints/getLatestDataFromGitLab.js
@@ -0,0 +1,5 @@
+import * as browser from "webextension-polyfill";
+
+export const getLatestDataFromGitLab = async () => {
+  console.log("Start fetching data");
+};
---
+++ src/components/TestAlert.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Alert, AlertIcon, AlertTitle } from "@chakra-ui/react";
+
+export default function TestAlert(props) {
+  return (
+    <Alert
+      status="success"
+      variant="subtle"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+    >
+      <AlertIcon boxSize="30px" mr={0} />
+      <AlertTitle fontSize="md">props.message</AlertTitle>
+    </Alert>
+  );
+}
