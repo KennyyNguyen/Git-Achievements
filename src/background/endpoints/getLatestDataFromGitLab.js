0423f0eb9ded988bc439ed42e58694d2833ae576
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-19 21:11:06 +0200

    WIP: fetch latest user data from GitLab

--- src/background/endpoints/getLatestDataFromGitLab.js
+++ src/background/endpoints/getLatestDataFromGitLab.js
@@ -1,5 +1,19 @@
 import * as browser from "webextension-polyfill";
+import { getSettings } from "../../common/getSettings";
+import { initGitlabApi } from "../../common/initGitlabApi";
 
 export const getLatestDataFromGitLab = async () => {
   console.log("Start fetching data");
+
+  const settings = await getSettings();
+  const gitLabApi = initGitlabApi(settings);
+  const currentUser = await gitLabApi.Users.current();
+
+  const userData = {
+    currentUser: currentUser,
+  };
+
+  await browser.storage.local.set({ userData });
+
+  return console.log("API fetched successfully!" + { userData });
 };
--- src/background/index.js
+++ src/background/index.js
@@ -2,11 +2,15 @@ import * as browser from "webextension-polyfill";
 
 console.log("background script loaded");
 
-const handleMessage = (message) => {
-  if (message.type === "ping") {
-    return Promise.resolve(true);
-  }
-  console.log("pong");
-};
+// const handleMessage = (message) => {
+//   if (message.type === "getLatestDataFromGitLab") {
+//     return Promise.resolve(true);
+//   }
+//   console.log("pong");
+// };
 
-browser.runtime.onMessage.addListener(handleMessage);
+browser.runtime.onMessage.addListener((message) => {
+  if (message.type === "getLatestDataFromGitLab") {
+    return Promise.resolve("pong");
+  }
+});
