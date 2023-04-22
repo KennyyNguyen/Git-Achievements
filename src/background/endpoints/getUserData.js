95b92d1fb103253dca0f528bbe27465ccbe6d0df
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-22 17:40:38 +0200

    Change constant name

--- src/background/endpoints/getUserData.js
+++ src/background/endpoints/getUserData.js
@@ -3,14 +3,14 @@ import { getSettings } from "../../common/getSettings";
 import { initGitlabApi } from "../../common/initGitlabApi";
 
 export const getUserData = async () => {
-  console.log("Start fetching data");
+  console.log("Start fetching user data");
 
   const settings = await getSettings();
   const gitLabApi = initGitlabApi(settings);
-  const currentUser = await gitLabApi.Users.current();
+  const currentUserData = await gitLabApi.Users.current();
 
-  const userMailRequest = currentUser.email;
-  const userIdRequest = currentUser.id;
+  const userMailRequest = currentUserData.email;
+  const userIdRequest = currentUserData.id;
 
   const [mail, id] = await Promise.all([userMailRequest, userIdRequest]);
 
