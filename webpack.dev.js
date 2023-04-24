b23d013dbcfd3e910610580afbf25af788404839
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-04-24 19:27:04 +0200

    Add environmental variable plugin to webpack, and add client for connection to database

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: {
@@ -41,6 +42,7 @@ module.exports = {
       filename: "popup.html",
     }),
     new MiniCssExtractPlugin(),
+    new Dotenv(),
     new CopyPlugin({
       patterns: [
         {
