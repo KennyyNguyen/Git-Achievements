236c4dea74613b748181b2389069185ba3cf051f
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-05-02 20:40:20 +0200

    Add state, submit handling and validation to form

--- src/components/CreateAchievementModal.js
+++ src/components/CreateAchievementModal.js
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -12,36 +8,79 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  VStack,
   useDisclosure,
 } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import TextInput from "./TextInput";
+import * as Yup from "yup";
 
 export default function CreateAchievementModal() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  const validationSchema = Yup.object({
+    code: Yup.number()
+      .max(9999, "Must be 4 digits or less")
+      .typeError("Code must be number")
+      .integer("Code must consist of only integers")
+      .required("Code is required"),
+    name: Yup.string().required("Name is required"),
+    description: Yup.string().required("Description is required"),
+  });
+
   return (
-    <Flex>
-      <Button onClick={onOpen}>Create achievement set</Button>
-      <Modal size="md" isOpen={isOpen} onClose={onClose} isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create an achievement set</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <FormControl>
-              <FormLabel>Code</FormLabel>
-              <Input placeholder="3214" />
-              <FormLabel>Name</FormLabel>
-              <Input placeholder="ITGK-project" />
-              <FormLabel>Description</FormLabel>
-              <Input placeholder="A school project in ITGK" />
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button>Create</Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Flex>
+    <Formik
+      initialValues={{ code: "", name: "", description: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        console.log(values);
+        actions.resetForm();
+      }}
+    >
+      {(props) => (
+        <>
+          <Button onClick={onOpen}>Create achievement set</Button>
+          <Modal
+            size="md"
+            isOpen={isOpen}
+            onClose={onClose}
+            scrollBehavior="inside"
+          >
+            <ModalOverlay backdropFilter="blur(10px)" />
+            <ModalContent>
+              <ModalHeader>Create an achievement set</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Form>
+                  <VStack spacing={7}>
+                    <TextInput
+                      label="Code"
+                      name="code"
+                      placeholder="Code to distribute"
+                    />
+                    <TextInput
+                      label="Name"
+                      name="name"
+                      placeholder="Name of achievement set"
+                    />
+                    <TextInput
+                      label="Description"
+                      name="description"
+                      placeholder="Short description of your project"
+                    />
+                  </VStack>
+                  <ModalFooter>
+                    <Button type="submit" mr="20px" onClick={onClose}>
+                      Create
+                    </Button>
+                    <Button onClick={onClose}>Cancel</Button>
+                  </ModalFooter>
+                </Form>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+      )}
+    </Formik>
   );
 }
