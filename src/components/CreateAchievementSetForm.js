dcdfa1abee50910702be41d16022cb12c70b8402
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-05-09 09:15:20 +0200

    Add achievement and achievement set forms

--- src/components/CreateAchievementSetForm.js
+++ src/components/CreateAchievementSetForm.js
@@ -1,49 +1,66 @@
-import React from "react";
-import { VStack } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { VStack, useToast } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import supabase from "../common/supabaseClient";
 import TextInput from "./TextInput";
 
 export default function CreateAchievementSetForm() {
+  const [insertStatus, setInsertStatus] = useState(null);
+  const [achievementId, setAchievementId] = useState("");
+
   const validationSchema = Yup.object({
-    code: Yup.number()
-      .max(9999, "Must be 4 digits or less")
-      .typeError("Code must be number")
-      .integer("Code must consist of only integers")
-      .required("Code is required"),
     name: Yup.string().required("Name is required"),
     description: Yup.string().required("Description is required"),
   });
 
+  const toast = useToast();
+
+  useEffect(() => {
+    if (insertStatus === true) {
+      toast({
+        title: "New achievement set created!",
+        description: `Write down the achievement ID: ${achievementId}`,
+        status: "success",
+        duration: 30000,
+        isClosable: true,
+      });
+      setInsertStatus(null);
+    } else if (insertStatus === false) {
+      toast({
+        title: "Error!",
+        description: "Could not create new achievement set",
+        status: "error",
+        duration: 30000,
+        isClosable: true,
+      });
+      setInsertStatus(null);
+    }
+  }, [insertStatus, toast, achievementId]);
+
   return (
     <Formik
-      initialValues={{ code: "", name: "", description: "" }}
+      initialValues={{ name: "", description: "" }}
       validationSchema={validationSchema}
       onSubmit={async (values, actions) => {
-        console.log(values);
         const { data, error } = await supabase
           .from("achievement_sets")
           .insert(values)
           .select();
         if (error) {
           console.log(error);
+          setInsertStatus(false);
         }
         if (data) {
-          console.log("This is the data inserted in the database:", data);
-          console.log("This is the id of the inserted object:", data[0].id);
+          setAchievementId(data[0].achievement_set_id);
           actions.resetForm();
+          setInsertStatus(true);
         }
       }}
     >
       {(props) => (
-        <Form id="achievementSetForm">
+        <Form id="achievementForm">
           <VStack spacing={7}>
-            <TextInput
-              label="Code"
-              name="code"
-              placeholder="Code to distribute"
-            />
             <TextInput
               label="Name"
               name="name"
