a6392aef99f0e9590b2540c8c03d20c0117c35bd
Kenny Nguyen <kennyn@stud.ntnu.no>
2023-05-23 13:55:16 +0200

    Make each row display three badges, and add progress bar to achievment badge modal

--- src/components/AchievementBadge.js
+++ src/components/AchievementBadge.js
@@ -11,36 +11,41 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
-  Button,
   useDisclosure,
+  Progress,
 } from "@chakra-ui/react";
 
 export default function AchievementBadge(props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <WrapItem display="flex" flexDirection="column" alignItems="center">
-      <Box as="Button" onClick={onOpen}>
-        <Avatar name={props.achievement.name} />
-      </Box>
-      <Box as="Button" onClick={onOpen}>
-        <Text fontSize="sm">{props.achievement.name}</Text>
-      </Box>
+    <>
+      <WrapItem
+        width="25%"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Avatar name={props.achievement.name} as="button" onClick={onOpen} />
+        <Text fontSize="sm" as="button" onClick={onOpen}>
+          {props.achievement.name}
+        </Text>
+      </WrapItem>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay backdropFilter="blur(10px)" />
-        <ModalContent>
+        <ModalContent flexDirection="column" alignItems="center" mt="1rem">
           <ModalHeader>{props.achievement.name}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Avatar name={props.achievement.name} />
-            <Text>Sint pariatur anim id eiusmod ut Lorem anim occaecat.</Text>
+            <Avatar size="xl" name={props.achievement.name} />
+            <Text>
+              Sint pariatur anim id eiusmod ut Lorem anim occaecat. Fugiat velit
+              labore reprehenderit dolor.
+            </Text>
+            <Progress value={80} />
           </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
+          <ModalFooter />
         </ModalContent>
       </Modal>
-    </WrapItem>
+    </>
   );
 }
